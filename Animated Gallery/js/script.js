f7c1fc25a839db837022acf81380a612b629e1be
MargoJunker <margo.junker@khk.ee>
2015-12-08 21:24:10 +0200

    Lecture 18: quicksand animation Part - B

--- Animated Gallery/js/script.js
+++ Animated Gallery/js/script.js
@@ -1,31 +1,60 @@
-$(document).ready(function()){
-    var items = $('#gallery li'),
-        itemsByTags =[];
+$(document).ready(function(){
+    var items = $('#gallery').find('li'),
+        itemsByTags = {};
 
-    //loop through tags
+    // Loop Through Tags
+
     items.each(function(i){
         var elem = $(this),
-        tags = elem.data('tags').split(',');
-        
-        //Add data attribute for quicksand
-        elem.attr('data-id',i);
-        
-        $.each(tags,function(key,value){
-            //Remove whitespace
-            valuse = $.trim(value);
-            
-            if(!(valuse in itemsByTags)){
-                //Aad emty value
-                itemsByTags[value] = [];
+            tags = elem.data('tags').split(',');
+        // Add data attribute for quicksand
+        elem.attr('data-id', i);
+
+        $.each(tags, function(key, value){
+            // Remove whitespace
+            value = $.trim(value);
+            if(!(value in itemsByTags)){
+                // Add empty value
+                itemsByTags[value]=[];
             }
-            
-            //Add image to arry
+            // Add image to array
             itemsByTags[value].push(elem);
         });
     });
+    // Create "All Items Function"
+    createList("All Items", items);
+    $.each(itemsByTags,function(k, v){
+        createList(k,v);
+    });
 
-    //Create "All Items" option
-    createList(itemsByTags, function(k, v){
-        createList(k, v);
-    })
+    // Click Handler
+
+    $(document).on('click',"#navbar a",function(e){
+        var link = $(this);
+
+        link.addClass('active').siblings().removeClass('active');
+        $('#gallery').quicksand(link.data('list').find('li'));
+        e.preventDefault();
+    });
+
+    $('#navbar').find(' a:first').click();
+
+    // Create the lists
+    function createList(text,items){
+        // Create empty ul
+        var ul =$('<ul>',{'class':'hidden'});
+        $.each(items,function(){
+           $(this).clone().appendTo(ul);
+        });
+
+        // Add gallery div
+        ul.appendTo('#gallery');
+
+        // Create menu item
+        var a = $('<a>',{
+            html:text,
+            href:'#',
+            data:{list:ul}
+        }).appendTo('#navbar')
+    }
 });
