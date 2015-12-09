72343622d6521298bf20db6c4bb07066fe8f2e1b
MargoJunker <margo.junker@khk.ee>
2015-12-09 03:54:26 +0200

    Lecture 47: Process Questions

--- Quiz/js/quiz.js
+++ Quiz/js/quiz.js
@@ -22,35 +22,81 @@ $(document).ready(function(){
     
     $('#q1 #submit').click(function(){
         $('.questionForm').hide();
+        proccess('q1');
         $('#q2').fadeIn(300);
         return false;
     });
     
     $('#q2 #submit').click(function(){
         $('.questionForm').hide();
+        proccess('q2');
         $('#q3').fadeIn(300);
         return false;
     });
     
     $('#q3 #submit').click(function(){
         $('.questionForm').hide();
+        proccess('q3');
         $('#q4').fadeIn(300);
         return false;
     });
     
     $('#q4 #submit').click(function(){
         $('.questionForm').hide();
+        proccess('q4');
         $('#q5').fadeIn(300);
         return false;
     });
     
     $('#q5 #submit').click(function(){
         $('.questionForm').hide();
+        proccess('q5');
         $('#result').fadeIn(300);
         return false;
     });
     
 });
+
+// Process the answers
+
+function process(q){
+    if(q == q1){
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted == sessionStorage.a1){
+            score ++;
+        }
+    }
+    if(q == q2){
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted == sessionStorage.a2){
+            score ++;
+        }
+    }
+    if(q == q3){
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted == sessionStorage.a3){
+            score ++;
+        }
+    }
+    if(q == q4){
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted == sessionStorage.a4){
+            score ++;
+        }
+    }
+    if(q == q5){
+        var submitted = $('input[name=q1]:checked').val();
+        if(submitted == sessionStorage.a5){
+            score ++;
+        }
+        $('#results').html('<h3>Your final score is: '+score+' out of 5</h3><a href="indef.html">Take Quiz Again</a>')
+    }
+    return false;
+}
+    
+    
+    
+    
 
 // Add event listener
 window.addEventListener('load', init, false);
