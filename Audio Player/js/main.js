b9e191d6a4f3e85b2570e3b35dfc289fb3a867b2
MargoJunker <margo.junker@khk.ee>
2015-12-09 19:50:15 +0200

    Lecture 56: Playlist Functionality Part - A

--- Audio Player/js/main.js
+++ Audio Player/js/main.js
@@ -1,7 +1,29 @@
-var audio = new Audio('media/Ride Out - Kid Ink, Tyga, Wale, YG & Rich Homie Quan.mp3');
+var audio;
 
 //Hide pause
 $('#pause').hide();
+
+initAudio('#playlist li:first-child');
+
+function initAudio(element) {
+    var song = element.attr('song');
+    var title = element.text();
+    var cover = element.attr('cover');
+    var artist = element.attr('artist');
+    
+    //Create audio object
+    audio = new Audio('media/'+ song);
+    
+    //Insert audio info
+    $('.artist').text(artist);
+    $('.title').text(title);
+    
+    //Insert song cover
+    $('img.cover').attr('src','images/covers/'+cover);
+    
+    $('#playlist li').removeClass('active');
+    element.addClass('active');
+}
 
 //Play button
 $('#play').click(function() {
