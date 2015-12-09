15f550763ac44ae860023f5e985603208af87e48
MargoJunker <margo.junker@khk.ee>
2015-12-09 22:09:21 +0200

    Lecture 65: Updating ToDos

--- Mobile ToDo App/js/main.js
+++ Mobile ToDo App/js/main.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
         $('#todos').listview('refresh');
     }
 
-
     // Add a Todo
     $('#add_form').submit(function () {
 
@@ -50,28 +49,46 @@ $(document).ready(function () {
             localStorage.setItem('todos', JSON.stringify(todos));
         }
     });
-    
-    //Delete Todo
-    $('#edit_form').on('click','#delete', function () {
+
+    // Edit Todo
+    $('#edit_form').submit(function () {
         currentTodoName = localStorage.getItem('currentTodoName');
         currentTodoDate = localStorage.getItem('currentTodoDate');
-        //Loop through todos
         for(var i = 0; i < todoList.length; i++){
             if(todoList[i].todo_name == currentTodoName){
                 todoList.splice(i,1);
             }
             localStorage.setItem('todos', JSON.stringify(todoList));
         }
-
-        // Close and go home
+        var todo_name_edit = $('#todo_name_edit').val();
+        var todo_date_edit = $('#todo_date_edit').val();
+        var todos = JSON.parse(localStorage.getItem('todos'));
+        var update_todo = {
+            "todo_name": todo_name_edit,
+            "todo_date": todo_date_edit
+        };
+        todos.push(update_todo);
+        localStorage.setItem('todos', JSON.stringify(todos));
+    });
+    
+    //Delete Todo
+    $('#edit_form').on('click','#delete', function () {
+        currentTodoName = localStorage.getItem('currentTodoName');
+        currentTodoDate = localStorage.getItem('currentTodoDate');
+        for(var i = 0; i < todoList.length; i++){
+            if(todoList[i].todo_name == currentTodoName){
+                todoList.splice(i,1);
+            }
+            localStorage.setItem('todos', JSON.stringify(todoList));
+        }
         $.mobile.changePage($('#home'),'pop');
     })
-    
+
     $('#todos').on('click', '#todo_link',function(){
         localStorage.setItem('currentTodoName',$(this).data('todo_name'));
         localStorage.setItem('currentTodoDate',$(this).data('todo_date'));
     });
-
+    // Insert current data into edit form
     $(document).on('pageshow','#edit', function () {
         currentTodoName = localStorage.getItem('currentTodoName');
         currentTodoDate = localStorage.getItem('currentTodoDate');
@@ -83,10 +100,10 @@ $(document).ready(function () {
     $(document).on('pageshow','#home', function () {
         window.location.reload();
     });
-    
-    //Clear Todos
+
+    // Clear Todos
     $('#clear_btn').click(function(){
         localStorage.clear();
     });
-    
+
 });
