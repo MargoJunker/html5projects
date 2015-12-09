db7dcb6c67c3805534e75eea60e23954815e362d
MargoJunker <margo.junker@khk.ee>
2015-12-09 22:03:56 +0200

    Lecture 64: Creating and Deleting ToDos Part - B

--- Mobile ToDo App/js/main.js
+++ Mobile ToDo App/js/main.js
@@ -1,24 +1,32 @@
-//Set todo list variable
+// Set todo list variable
 var todoList = JSON.parse(localStorage.getItem('todos'));
 
-$(document).ready(function() {
-    //Set counter
+$(document).ready(function () {
+
+    // Set Ccounter
     var i = 0;
-    //Check for todos
-    if(localStorage.getItem('todos') != null) {
-        //Loop through and output li items
-        $.each(todoList,function(key, value) {
-            $('#todos').prepend('<li id="task-'+i+'"><a id"todo_list" href="#edit" data-todo_name="'+value.todo_name+'" data-todo_date="'+value.todo_date+'">'+value.todo_name+'</a></li>');
-            i++;
+
+    // Check for todos
+    if(localStorage.getItem('todos') != null){
+        items='';
+        // Loop through and output li items
+        $.each(todoList, function (key,value) {
+            items='<li id="task-'+i+'"><a id="todo_link" href="#edit" data-todo_name="'+value.todo_name+'" data-todo_date="'+value.todo_date+'">'+value.todo_name+'<span>'+value.todo_date+'</span></a></li>'+items;
+            i++
         });
-        //Refresh
+        $('#todos').html(items);
+        // Refresh
         $('#todos').listview('refresh');
     }
+
+
     // Add a Todo
     $('#add_form').submit(function () {
+
         // Get submitted values
         var todo_name = $('#todo_name').val();
         var todo_date = $('#todo_date').val();
+
         // Simple field validation
         if (todo_name == '') {
             alert('Please give the todo a name');
@@ -26,23 +34,54 @@ $(document).ready(function() {
             alert('Please add date');
         } else {
             var todos = JSON.parse(localStorage.getItem('todos'));
+
             //Check todos
             if (todos == null) {
                 todos = [];
             }
+
             // Create array with new todo
             var new_todo = {
                 "todo_name": todo_name,
                 "todo_date": todo_date
             }
+
             todos.push(new_todo);
             localStorage.setItem('todos', JSON.stringify(todos));
         }
     });
     
+    //Delete Todo
+    $('#edit_form').on('click','#delete', function () {
+        currentTodoName = localStorage.getItem('currentTodoName');
+        currentTodoDate = localStorage.getItem('currentTodoDate');
+        //Loop through todos
+        for(var i = 0; i < todoList.length; i++){
+            if(todoList[i].todo_name == currentTodoName){
+                todoList.splice(i,1);
+            }
+            localStorage.setItem('todos', JSON.stringify(todoList));
+        }
+
+        // Close and go home
+        $.mobile.changePage($('#home'),'pop');
+    })
+    
     $('#todos').on('click', '#todo_link',function(){
         localStorage.setItem('currentTodoName',$(this).data('todo_name'));
         localStorage.setItem('currentTodoDate',$(this).data('todo_date'));
+    });
+
+    $(document).on('pageshow','#edit', function () {
+        currentTodoName = localStorage.getItem('currentTodoName');
+        currentTodoDate = localStorage.getItem('currentTodoDate');
+        $('#edit_form input[name=todo_name_edit]',this).val(currentTodoName);
+        $('#edit_form input[name=todo_date_edit]',this).val(currentTodoDate);
+
+    });
+
+    $(document).on('pageshow','#home', function () {
+        window.location.reload();
     });
     
     //Clear Todos
